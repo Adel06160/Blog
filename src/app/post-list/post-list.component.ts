ea26612357f5631a4936e165ffc46940f388ee71
Adel06160 <44773969+Adel06160@users.noreply.github.com>
2018-11-05 15:49:24 +0100

    Update post-list.component.ts

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  templateUrl: './post-list.component.html'
 })
 export class PostListComponent implements OnInit {
 
