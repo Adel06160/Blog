89b60c6b1ae4bce55047a1ab30ad4abd6c56df0f
Adel06160 <44773969+Adel06160@users.noreply.github.com>
2018-11-05 15:50:52 +0100

    Update post-list-item.component.ts

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
-  templateUrl: './post-list-item.component.html',
-  styleUrls: ['./post-list-item.component.css']
+  templateUrl: './post-list-item.component.html'
 })
 export class PostListItemComponent implements OnInit {
   @Input() title: string;
